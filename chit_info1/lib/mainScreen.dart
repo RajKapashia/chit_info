0cf7e77af09ac8fc1c12b29ba442f6a43d57cb51
RajKapashia <rjkapashia@gmail.com>
2019-07-19 00:20:40 +0530

    adding mscreen

---
+++ chit_info1/lib/mainScreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart'
+
+class mScreen extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return mScreenState();
+  }
+
+}
+class mScreenState extends State<mScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+}
