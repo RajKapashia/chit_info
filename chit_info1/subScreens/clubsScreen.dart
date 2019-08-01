f6ad511f26fa1e9a07cb591a35cb3d3affe22a01
Rachit Plah <rachitplah@outlook.com>
2019-08-01 23:50:37 +0530

    added new screen

---
+++ chit_info1/subScreens/clubsScreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class clubsScreen extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return clubsScreenState();
+  }
+}
+class clubsScreenState extends State<clubsScreen>{
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
